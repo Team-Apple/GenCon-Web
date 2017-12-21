06057cf22108d5558bc13523aeb7b3ebbc4a395c
Kai Kitamura <namedpython@gmail.com>
2017-12-21 11:33:29 +0900

    tasks api completed

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -62,7 +62,6 @@ class Api::EventsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
-      puts @event.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,71 +1,71 @@
-# class Api::TasksController < ApplicationController
-#   before_action :set_task, only: [:update]
-#   protect_from_forgery :except => [:create, :update, :destroy]
-#
-#   # GET /tasks
-#   # GET /tasks.json
-#   def index
-#     if params['start_from_date']
-#       @tasks = Task.where(date: params['start_from_date'])
-#     end
-#       @tasks = Task.all
-#     end
-#
-#     respond_to do |format|
-#       format.html { render html: '200'}
-#       format.json { render 'tasks/index', tasks: @tasks, status: :ok }
-#     end
-#   end
-#
-#   # POST /tasks
-#   # POST /tasks.json
-#   def create
-#     @task = Task.new(task_params)
-#
-#     respond_to do |format|
-#       if @task.save
-#         format.html { render html: '200' }
-#         format.json { render 'tasks/task', status: :created, tasks: @tasks }
-#       else
-#         format.html { render html: '500', status: :unprocessable_entity }
-#         format.json { render json: @task.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-#
-#   # PATCH/PUT /tasks/1
-#   # PATCH/PUT /tasks/1.json
-#   def update
-#     respond_to do |format|
-#       if @task.update(task_params)
-#         format.html { render html: '200', status: :ok }
-#         format.json { render 'tasks/task', status: :ok, task: @task }
-#       else
-#         format.html { render html: '500', status: :unprocessable_entity }
-#         format.json { render json: @task.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-#
-#   # DELETE /tasks/1
-#   # DELETE /tasks/1.json
-#   def destroy
-#     @task = Task.find(params[:id])
-#     @task.destroy
-#     respond_to do |format|
-#       format.html { render html: '200', status: :ok }
-#       format.json { head :no_content }
-#     end
-#   end
-#
-#   private
-#     # Use callbacks to share common setup or constraints between actions.
-#     def set_task
-#       @task = Task.find(params[:id])
-#     end
-#
-#     # Never trust parameters from the scary internet, only allow the white list through.
-#     def task_params
-#       params.permit(:title, :start_from_date, :start_from_time, :deadline_date, :deadline_time, :memo, :priority)
-#     end
-# end
+class Api::TasksController < ApplicationController
+  before_action :set_task, only: [:update]
+  protect_from_forgery :except => [:create, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    if params['start_from_date']
+      @tasks = Task.where(start_from_date: params['start_from_date'])
+    else
+      @tasks = Task.all
+    end
+
+    respond_to do |format|
+      format.html { render html: '200'}
+      format.json { render 'tasks/index', tasks: @tasks, status: :ok }
+    end
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { render html: '200' }
+        format.json { render 'tasks/task', status: :created, task: @task }
+      else
+        format.html { render html: '500', status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { render html: '200', status: :ok }
+        format.json { render 'tasks/task', status: :ok, task: @task }
+      else
+        format.html { render html: '500', status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    respond_to do |format|
+      format.html { render html: '200', status: :ok }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.permit(:title, :start_from_date, :start_from_time, :deadline_date, :deadline_time, :memo, :priority)
+    end
+end
