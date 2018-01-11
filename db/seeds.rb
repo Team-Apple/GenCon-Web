285fac04f70ca9f264bd6aa60d1fd992f9d92b39
北村 開 <namedpython@gmail.com>
2018-01-11 12:10:59 +0900

    why heroku died
    i dont know

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 Event.delete_all
 
 base_a = %w[彼女 友達 家族]
